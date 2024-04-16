e070981a8271a2a2cfd6829df6190cc6375d3e0c
idonotmatch <161400056+idonotmatch@users.noreply.github.com>
2024-04-16 18:32:28 -0400

    update to URI routing

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,16 +1,23 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+    const dbURI = process.env.MONGODB_URI; // Use an environment variable for the MongoDB URI
+    if (!dbURI) {
+        console.error('MongoDB connection string is missing in the environment variables.');
+        process.exit(1); // Exit if the MongoDB URI is not set
+    }
+
     try {
-        // Replace 'your_mongodb_connection_string' with your actual connection string
-        const conn = await mongoose.connect('mongodb+srv://dang:<12QA34ws!@>@testcluster.ep8qdjk.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster', {
+        const conn = await mongoose.connect(dbURI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useCreateIndex: true, // Depending on your Mongoose version, you may not need this
+            useFindAndModify: false // Depending on your Mongoose version, you may not need this
         });
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
-        console.error(`Error: ${err.message}`);
+        console.error(`MongoDB Connection Error: ${err.message}`);
         process.exit(1); // Exit process with failure
     }
 };
